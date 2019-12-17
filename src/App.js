238d1e0f366e5ff99f766d0a859c3a5032db112e
Eliascm17 <Elias.cmoreno17@gmail.com>
2019-12-17 13:55:38 -0600

    maybe a gradient is the way?

--- src/App.js
+++ src/App.js
@@ -4,30 +4,34 @@ import './App.css';
 import Projects from './components/Projects'
 
 const Text = () => (
-    <div className='landingContainer'>
-        <h1 className='Name'>Hi, I'm Elias</h1>
-    </div>
+  <header>
+    <h1 className="Name">Hi, I'm Elias</h1>
+    <img
+      src="http://erikdkennedy.com/r-r-r-random/divider-triangle.png"
+      class="divider"
+    />
+  </header>
 );
 
-const Triangle = () => (
+// const Triangle = () => (
   //   <div>
   //     <div className="triangle1"></div>
   //     <div className="square"></div>
   //     <div className="triangle2"></div>
   //   </div>
 
-  <svg viewBox="10 10 167 281" version="1.1">
-    <polygon className="parallelogram" points="0,226 83.5,0.1 167,226 " />
-  </svg>
+//   <svg viewBox="10 10 167 281" version="1.1">
+//     <polygon className="parallelogram" points="0,226 83.5,0.1 167,226 " />
+//   </svg>
 
-);   
+// );   
 
 function App() {
   return (
     <Fragment>
       <Text />
-      <Triangle />
-      <Projects />
+      {/* <Triangle /> */}
+      {/* <Projects /> */}
     </Fragment>
   );
 }
